29b4d0eb073356a1957d8f6d1c363cd529131b20
Aykut Kırcan <aykutkircan@icloud.com>
2021-11-12 16:28:50 +0300

    eslint

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-
-const { SayHello, PostaGuvercini } = require('./lib/posta-guvercini')
-
-const options = {
-  username: 'Aykut',
-  password: 'ad2sf4%&',
-  sender: 11,
-  baseUrl: 12
-}
-
-const sayHello = new SayHello('x', 'y')
-const postaGuvercini = new PostaGuvercini(options)
-
-postaGuvercini.sendSimpleSms()
-// sayHello.sayHello()
+export { PostaGuvercini } from './lib/posta-guvercini'
